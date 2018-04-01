72f1689bbc815246e500851ef4b44da25880187c
Liang Chen (Wuwulou, seu) <tongust@163.com>
2018-04-01 22:52:25 +0800

    Bug in scripts. Forget cases: reverse complementary

--- script/test-sbwt/check_position.py
+++ script/test-sbwt/check_position.py
@@ -1,6 +1,15 @@
 from sys import argv
 import os
 
+def reverse_complement(s):
+        mp = {'A': 'T', 'C':'G', 'T':'A', 'G':'C', 'N':'A'}
+        st = list(s)
+        size = len(s)
+        t = ['A'] * size
+        for i in range(0, size):
+                t[size-1-i] = mp[s[i]]
+        return ''.join(t)
+
 if __name__ == '__main__':
         if len(argv) != 3:
                 print "usage: [alignment files] [reference files]"
@@ -11,14 +20,17 @@ if __name__ == '__main__':
                         if ml.startswith('>'):
                                 continue
 
-                        ml.rstrip(os.linesep)
+                        ml = ml.rstrip(os.linesep)
                         ref += ml
         with open (argv[1]) as mf:
                 for ml in mf:
                         ml.rstrip(os.linesep)
-                        read = ml.split('\t')[4]
+                        read = ml.split('\t')[4].rstrip(os.linesep)
                         pos  = int(ml.split('\t')[3])
+                        flag = ml.split('\t')[1]
                         s = ref[pos:pos+len(read)]
+                        if flag == '-':
+                                read = reverse_complement(read)
                         count = 0
                         for j in range(0, len(read)):
                                 if read[j] != s[j]:
